447275832dab751bc4522768975c8146f12d5d5a
emendozag <esteban.mendoza@farmatodo.com>
2023-07-12 22:05:16 -0500

    12 Julio

---
+++ funciones.py
@@ -0,0 +1,9 @@
+import this
+
+print("Hola estoy trabajando funciones en entornos y Git")
+
+def my_print():
+    print("Esta es mi funcion")
+    print("Esta es mi funcion de nuevo")
+my_print()
+    
