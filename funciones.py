bb8b94043d7e9357994e630541f799b4be002bf9
emendozag <esteban.mendoza@farmatodo.com>
2023-07-13 21:25:14 -0500

    13 Julio segundo commit

--- funciones.py
+++ funciones.py
@@ -2,5 +2,17 @@ import this
 
 def suma (a,b):
     c = a + b
-    print(c)
+    print("La suma de a + b es: ", c)
+
+def multi (a,b):
+    c = a * b
+    print("La suma de a + b es: ", c)
+
+def resta (a,b):
+    c = a - b
+    print("La suma de a + b es: ", c)
+
+suma(10,5)
+multi(10,5)
+resta(10,5)
     
