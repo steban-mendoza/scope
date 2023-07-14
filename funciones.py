3dd7ba405e6c1e0f611625932fe257af7f224eae
emendozag <esteban.mendoza@farmatodo.com>
2023-07-13 21:14:28 -0500

    funcion 13 julio

--- funciones.py
+++ funciones.py
@@ -1,9 +1,6 @@
 import this
 
-print("Hola estoy trabajando funciones en entornos y Git")
-
-def my_print():
-    print("Esta es mi funcion")
-    print("Esta es mi funcion de nuevo")
-my_print()
+def suma (a,b):
+    c = a + b
+    print(c)
     
