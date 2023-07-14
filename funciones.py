69bdce476e983e64c9328f96a26b059811f5ce36
emendozag <esteban.mendoza@farmatodo.com>
2023-07-13 21:54:47 -0500

    nueva rama 13 julio

--- funciones.py
+++ funciones.py
@@ -15,4 +15,32 @@ def resta (a,b):
 suma(10,5)
 multi(10,5)
 resta(10,5)
+
+# variables locales
+def imprimir_nombre():
+    mi_nombre="Esteman"
+    print("El nombre de la funcion es: ", mi_nombre, id(mi_nombre), sep="-->")
+imprimir_nombre()
+#print("El nombre de la variable local es: ", mi_nombre)
+
+# Variables enclosing
+mi_nombre = "Esteban"
+
+def imprimir_nombre():
+    global mi_nombre
+    mi_nombre="Esteman"
+    print("El nombre de la funcion es: ", mi_nombre, id(mi_nombre), sep="-->")
+
+print("El nombre fuera de la funcion es: ", mi_nombre, id(mi_nombre), sep="-->")
+imprimir_nombre()
+
+# Variable global
+
+mi_nombre = "Esteban"
+def imprimir_nombre():
+    print("El nombre de la funcion es: ", mi_nombre, id(mi_nombre), sep="-->")
+
+print("El nombre fuera de la funcion es: ", mi_nombre, id(mi_nombre), sep="-->")
+imprimir_nombre()
+
     
